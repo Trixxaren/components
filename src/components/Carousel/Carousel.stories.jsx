95bea8a4bed950c3b3542209093c6e34b263936a
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-10-27 15:32:02 +0100

    Carousel component added

---
+++ src/components/Carousel/Carousel.stories.jsx
@@ -0,0 +1,15 @@
+import Carousel from "./Carousel";
+
+export default {
+  title: "Components/Carousel",
+  component: Carousel,
+  tags: ["autodocs"],
+};
+
+export const Default = () => {
+  return (
+    <div>
+      <Carousel.jsx />
+    </div>
+  );
+};
